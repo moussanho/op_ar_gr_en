873221e845c537216a6ffbd019f42751c3f16ad5
BILA <mohamedbila41@gmail.com>
2017-12-23 22:02:54 +0000

    DailyFirstCommit-ByBILA

--- alpha.hpp
+++ alpha.hpp
@@ -1,11 +1,21 @@
-//Structure de base ; le type int redefinie
+//Basic structure of our new int type 
 typedef struct int_t{
-	bool sign;
-	size_t size;//utilisation d'un type plus adaptée pour quantifier une dimension
-	int* val;
-}int_t;
-//Fonction d'initialisation
-void initInt(int_t & myInt, size_t size,bool sign) ; //Passage par reference de la structure;critère de performance
-//Redefinition des opérateurs de base par le mecanisme de surcharge
-//Opérateur d'addition
-
+	size_t register_size;
+	bool* register_content;
+} int_t;
+int_t* init_new_register(int_t* myStruct,size_t size){
+	assert((int(size)%32)!=0) ; 
+	int_t* myRegStruct=(int_t*)malloc(sizeof(int_t));
+	myRegStruct->register_size=size;
+	myRegStruct->register_content=(bool*)malloc(size);
+}
+int_t* addition(int_t* a,int_t* b){
+	assert(a->regsiter_size!=b->register_size);
+	int_t* result=(int_t*)malloc(sizeof(int_t));
+	result->register_size=a->register_size;
+	int counter;
+	for(counter=0;counter<result->register_size;counter++){
+		result->register_content[i] = (a->register_content[i]==b->register_content[i]) ? 0 : 1 ; 
+	}
+	return result;
+}
--- beta.cpp
+++ beta.cpp
@@ -4,8 +4,7 @@ int main(int argc, char** argv){
 	printf("Projet M1 CHPS");
 }
 
-void initInt(int_t & myInt, size_t size,bool sign){
-     myInt.val=(int*)malloc(int(s)*sizeof(int));
-     myInt.size = size;
-	 myInt.sign=sign ;
+void initInt(int_t *myInt, size_t s){
+     myInt->val=(int*)malloc(int(s)*sizeof(int));
+     myInt->size = s;
 }
